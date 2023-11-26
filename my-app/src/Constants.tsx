27ba09e9236587d60be603a317bc3b9bc7a1ef15
Nagarjuna Punna <nagarjuna.punna@zemosolabs.com>
2023-11-26 23:15:39 +0530

    feat: Added images via svg

---
+++ my-app/src/Constants.tsx
@@ -0,0 +1,77 @@
+import React from "react";
+
+import { ReactComponent as Add } from "../src/assets/icons/add.svg";
+import { ReactComponent as CareerAndSuccess } from "../src/assets/icons/career-and-sucess.svg";
+import { ReactComponent as Checked } from "../src/assets/icons/checked.svg";
+import { ReactComponent as CommunicationSkills } from "../src/assets/icons/communication-skills.svg";
+import { ReactComponent as CorperateCulture } from "../src/assets/icons/corperate-culture.svg";
+import { ReactComponent as DownArrow } from "../src/assets/icons/down-arrow.svg";
+import { ReactComponent as Economics } from "../src/assets/icons/economics.svg";
+import { ReactComponent as Edit } from "../src/assets/icons/edit.svg";
+import { ReactComponent as Entrepreneurship } from "../src/assets/icons/entrepreneurship.svg";
+import { ReactComponent as MotivationAndInspiration } from "../src/assets/icons/motivation-and-inspiration.svg";
+import { ReactComponent as NatureAndTheEnvironment } from "../src/assets/icons/nature-and-the-environment.svg";
+import { ReactComponent as Politics } from "../src/assets/icons/politics.svg";
+import { ReactComponent as ReadAgain } from "../src/assets/icons/read-again.svg";
+import { ReactComponent as RemoveTwo } from "../src/assets/icons/remove-two.svg";
+import { ReactComponent as Search } from "../src/assets/icons/search.svg";
+import { ReactComponent as SexAndRelationship } from "../src/assets/icons/sex-and-relationship.svg";
+import { ReactComponent as Timer } from "../src/assets/icons/timer.svg";
+import { ReactComponent as Unchecked } from "../src/assets/icons/unchecked.svg";
+import { ReactComponent as UpArrow } from "../src/assets/icons/up-arrow.svg";
+import { ReactComponent as Upload } from "../src/assets/icons/upload.svg";
+
+import { ReactComponent as Two } from "../src/assets/images/2.svg";
+import { ReactComponent as Three } from "../src/assets/images/3.svg";
+import { ReactComponent as Four } from "../src/assets/images/4.svg";
+import { ReactComponent as Five } from "../src/assets/images/5.svg";
+import { ReactComponent as Six } from "../src/assets/images/6.svg";
+import { ReactComponent as Seven } from "../src/assets/images/7.svg";
+import { ReactComponent as Eight } from "../src/assets/images/8.svg";
+import { ReactComponent as Nine } from "../src/assets/images/9.svg";
+import { ReactComponent as Ten } from "../src/assets/images/10.svg";
+import { ReactComponent as One } from "../src/assets/images/1.svg";
+import { ReactComponent as Banner } from "../src/assets/images/entrepreneurship.svg";
+
+import { ReactComponent as Blinkist } from "../src/assets/logos/logo.svg";
+
+export const Illustrations: { [key: string]: JSX.Element } = {
+  2: <Two />,
+  3: <Three />,
+  4: <Four />,
+  5: <Five />,
+  6: <Six />,
+  7: <Seven />,
+  8: <Eight />,
+  9: <Nine />,
+  10: <Ten />,
+  1: <One />,
+  banner: <Banner />,
+};
+
+export const Icons: { [key: string]: JSX.Element } = {
+  add: <Add />,
+  careerAndSuccess: <CareerAndSuccess />,
+  checked: <Checked />,
+  communicationSkills: <CommunicationSkills />,
+  corperateCulture: <CorperateCulture />,
+  downArrow: <DownArrow />,
+  economics: <Economics />,
+  edit: <Edit />,
+  entrepreneurship: <Entrepreneurship />,
+  motivationAndInspiration: <MotivationAndInspiration />,
+  natureAndTheEnvironment: <NatureAndTheEnvironment />,
+  politics: <Politics />,
+  readAgain: <ReadAgain />,
+  removeTwo: <RemoveTwo />,
+  search: <Search />,
+  sexAndRelationship: <SexAndRelationship />,
+  timer: <Timer />,
+  unchecked: <Unchecked />,
+  upArrow: <UpArrow />,
+  upload: <Upload />,
+};
+
+export const Logo: { [key: string]: JSX.Element } = {
+  blinkist: <Blinkist />,
+};
---
+++ my-app/src/components/atoms/Illustration/index.tsx
@@ -0,0 +1,27 @@
+import { Box, styled } from "@mui/material";
+import React from "react";
+import { Illustrations } from "../../../Constants";
+
+export interface IllustrationProps {
+  name: "2" | "3" | "4" | "5" | "6" | "7" | "8" | "9" | "10" | "1" | "banner";
+
+  margin?: string;
+}
+
+const StyledDiv = styled(Box)<{ margin?: string }>`
+  margin: ${(props) => props.margin};
+`;
+
+const Illustration: React.FC<IllustrationProps> = ({ name, margin }) => {
+  return (
+    <StyledDiv
+      data-testid={`illustration-${name}`}
+      className="illustration"
+      margin={margin}
+    >
+      {Illustrations[name]}
+    </StyledDiv>
+  );
+};
+
+export default Illustration;
